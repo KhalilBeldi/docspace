95abe0c24bd0a710b0e53156e1b5e65fb35b7a2e
Khalil Beldi <khalilbeldi17@gmail.com>
2025-04-23 17:04:58 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { PanelModule } from 'primeng/panel';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ButtonModule,PanelModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'docspaceclientattempt';
+  binary = true;
 }
