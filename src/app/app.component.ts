26c657baec9112a5877c65687dc61eb187a7a963
Khalil Beldi <khalilbeldi17@gmail.com>
2025-04-25 23:54:07 +0100

    style rectif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { PanelModule } from 'primeng/panel';
+import { ToastModule } from 'primeng/toast';
+import { StepperModule } from 'primeng/stepper';
+
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ButtonModule,PanelModule ],
+  imports: [RouterOutlet,ButtonModule,PanelModule,ToastModule,StepperModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [MessageService]
 })
 export class AppComponent {
   title = 'docspaceclientattempt';
-  binary = true;
+
+  constructor(private messageService: MessageService) {}
+
+
 }
+
+
